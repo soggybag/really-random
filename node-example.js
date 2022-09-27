de8eef891e286a0d441cf1f81d3210910aaa7e5d
Mitchell <soggybag@gmail.com>
2022-09-26 17:54:56 -0700

    updated

---
+++ node-example.js
@@ -0,0 +1,12 @@
+// const { random, dieX } = require('./dist/index.node.js')
+const { random, dieX } = require('./dist/index.js')
+
+// console.log(random(6))
+
+// const d6 = dieX(6)
+// console.log(d6())
+// console.log(d6())
+// console.log(d6())
+
+const d6 = dieX(6)
+console.log(d6())
